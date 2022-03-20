5a72d2faa1a7536c723d772d2b97d4571fd5712a
Paul <z9099630061@yandex.ru>
2022-03-20 22:55:36 +0300

    added comments

--- main.c
+++ main.c
@@ -5,26 +5,30 @@
 #include <unistd.h>
 #include <string.h>
 
-#define MAXNAME 256
-#define LENGTH 256
+#define MAXNAME 256	//max filename length
+#define LENGTH 256	//max length of comand
 
-char cwd[MAXNAME];
-char command[LENGTH] = {""};
-char **args;
+char cwd[MAXNAME];				//current directory
+char command[LENGTH] = {""};	//command buffer
+char **args;					//array with command and arguments
 
+//split command by spaces
 char **split(char *cmd, char **buf) {
 	char *tmp;
 	int cnt = 1;
 	int i = 0;
+	//counting words
 	while(cmd[i] != '\0' && cmd[i] != '\n') {
 		if (cmd[i] == ' ')
 			cnt++;
 		i++;
 	}
+	//reallocate memory for args
 	free(buf);
 	buf = (char**)malloc((cnt + 1) * sizeof(char*));
 	int j = 0;
-
+	
+	//using strtok for spliting command
 	tmp = strtok(cmd, " ");
 	while(tmp != NULL) {
 		buf[j] = tmp;
@@ -43,13 +47,18 @@ int main() {
 		if(getcwd(cwd, MAXNAME) == NULL)
 			perror("getcwd() error\n");
 		printf("%s$ ", cwd);
+
+		//reading command
 		fgets(command, LENGTH, stdin);
 		if (command[strlen(command) - 1] == '\n')
 			command[strlen(command) - 1] = '\0';
 		if(strcmp(command, "exit") == 0)
 			break;
+
 		args = (char**)malloc(sizeof(char*));
 		args = split(command, args);
+
+		//realizing 'cd' command
 		if(strcmp(args[0], "cd") == 0 && args[1] != NULL) {
 			int err = chdir(args[1]);
 			free(args);
@@ -58,6 +67,7 @@ int main() {
 			continue;
 		}
 
+		//fork main process for execvp
 		int p = fork();
 		if(p == 0) {
 			execvp(args[0], args);
@@ -65,6 +75,7 @@ int main() {
 			exit(0);
 		}
 		free(args);
+		//waiting for child process end otherwise it will corrupt output
 		wait(NULL);
 	}
 
